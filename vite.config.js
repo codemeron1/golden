e87ccbfe612662d83df3435bfffe3d6c497a36b4
codemeron <wondelrosario@gmail.com>
2025-12-29 14:27:11 +0800

    config: enable '@' sa pag-import

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
   base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 5176,
-    strictPort: true,
-    cors: true
   },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     emptyOutDir: true,
-    rollupOptions: {
-      external: ['electron']
-    }
-  }
-})
+  },
+});
